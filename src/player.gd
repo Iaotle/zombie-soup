fe4f48d0a506407970125aa88756dcb255ef8c1b
dscrtdv <ti.discretedev@gmail.com>
2025-06-09 00:36:39 +0200

    game over

--- src/player.gd
+++ src/player.gd
@@ -28,6 +28,21 @@ func game_over():
 	var center = get_viewport_rect().size / 2
 	label.position = center - (label.get_size() / 2)
 	add_child(label)
+	var timer = Timer.new()
+	timer.wait_time = 5.0  # Set the timer to 5 seconds
+	timer.one_shot = true  # Make the timer one-shot
+
+	# Connect the timeout signal to a function
+	timer.timeout.connect(_on_timer_timeout)
+
+	# Add the timer to the scene tree and start it
+	add_child(timer)
+	timer.start()
+
+func _on_timer_timeout():
+	# This function will be called when the timer times out
+	get_tree().paused = false
+	get_tree().reload_current_scene()
 		
 func update_heart():
 	for i in range(heart_list.size()):
