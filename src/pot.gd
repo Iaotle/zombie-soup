fc76a9c8981d554cfe06bcca5d38c24ec20b6644
Vadim Isakov <vintersiaotle@gmail.com>
2025-06-09 13:29:06 +0200

    center label, etc

--- src/pot.gd
+++ src/pot.gd
@@ -16,35 +16,42 @@ signal content_added
 signal food_spawned
 
 func _ready() -> void:
-	brain_broth.init_receipe("BrainBonesBroth", 5, ["Brain", "Bone"]);
-	eye_soup.init_receipe("EyeSoup", 2, ["Eye", "Ant"]);
+	brain_broth.init_receipe("BrainBonesBroth", 5, ["brains", "bones"]);
+	eye_soup.init_receipe("EyeSoup", 2, ["eye", "ant"]);
 	#emit_signal("content_added");
 	$CookingTimer.hide()
 	sprite.play("default");
 
 func content_check():
+	print('abcd', contents)
 	for i in contents:
 		match i:
-			"Eye":
+			"eye":
 				has_eye = true
-			"Ant":
+			"ant":
 				has_ant = true
-			"Brain":
+			"brains":
 				has_brain = true
-			"Bone":
+			"bones":
 				has_bone = true
 	if contents.size() == 2:
+		contents.clear()
 		if has_ant and has_eye:
 			spawn_bowl("Eye and Ants Soup")
-			return 
 		elif has_brain and has_bone:
 			spawn_bowl("Brains and Bones Broth")
-			return
-	spawn_bowl("Garbage")
+		else:
+			spawn_bowl("Garbage")
+		has_eye = false
+		has_ant = false
+		has_brain = false
+		has_bone = false
+		return
 
 func spawn_bowl(content : String):
 	var food = bowl.instantiate()
 	food.content = content
+	food.get_node("CenterContainer/Label").text = content
 	add_child(food)
 	emit_signal("food_spawned")
 	
